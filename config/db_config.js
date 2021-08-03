adcfb930cf4b65f50ac302be9904f52805be8692
soohyun <soohyun6879@naver.com>
2021-08-03 12:30:35 +0900

    init

---
+++ config/db_config.js
@@ -0,0 +1,30 @@
+// 데이터베이스 접속 정보
+
+var mysql = require('mysql');
+require('dotenv').config();
+
+const db_info = {
+    // 로컬환경
+	dev:{
+		host: 'localhost',
+		user: 'test',
+		password: process.env.DB_SECRET,
+		database: 'fermata'
+    },
+    // 실제 운영 서버 환경
+	real: {
+		host: '',
+        port: '',
+        user: '',
+        password : '',
+        database : ''
+	}	
+};
+
+const db_connection = {
+	init : function(){
+        return mysql.createConnection(db_info.dev);
+	}
+};
+
+module.exports = db_connection;
