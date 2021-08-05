c8010c8dd8d912b445d0bd607890bc41daa07fea
soohyun <soohyun6879@naver.com>
2021-08-05 19:45:19 +0900

    Add: getmusic(recent, times, alphabet)

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var musicRecentRouter = require('./routes/getmusic_recent'); // 음악 최신순 라우터
+var musicTimesRouter = require('./routes/getmusic_times'); // 음악 많이 재생한 순 라우터
+var musicAlphabetRouter = require('./routes/getmusic_alphabet.js'); // 음악 가나다순 라우터
 
 var app = express();
 
@@ -19,6 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/music', musicRecentRouter);
+app.use('/music', musicTimesRouter);
+app.use('/music', musicAlphabetRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/getmusic_times.js
@@ -0,0 +1,24 @@
+// 음악 많이 재생한 순으로 가져오기
+//Author : Soohyun, Last Modified : 2021.08.05
+
+var express = require('express');
+var router = express.Router();
+var config = require('../config/db_config');
+var connection = config.init()
+connection.connect();
+
+router.post('/times', function (req, res) {
+    var query = 'SELECT * FROM music ORDER BY count desc'; // 음악 많이 재생한 순 쿼리문
+
+    connection.query(query, function (error, result) {
+        if(error) { // 에러 발생시
+            console.log("error ocurred: ", error);
+            res.json({"code" : 400, "result": "error ocurred"});
+        } else {
+            console.log("music times success");
+            res.json({ "code": 200, "music": result });
+        }
+    });
+});
+
+module.exports = router;
