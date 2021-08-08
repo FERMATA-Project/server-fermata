cdd1c566aa6db1361c5e3a00b69b0445e953225c
name명 <dnri33@naver.com>
2021-08-08 22:12:01 +0900

    210808

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var playlistRouter = require('./routes/playlist');  //재생목록 라우터
 
 var app = express();
 
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/music', playlistRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
