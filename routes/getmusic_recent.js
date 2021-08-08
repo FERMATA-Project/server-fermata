0b13f5e8d5cfcfef42d873dcaa62efa9e9454a0a
soohyun <soohyun6879@naver.com>
2021-08-08 11:41:44 +0900

    0808

--- routes/getmusic_recent.js
+++ routes/getmusic_recent.js
@@ -13,12 +13,10 @@ router.post('/recent', function (req, res) {
     connection.query(query, function (error, result) {
         if(error) { // 에러 발생시
             console.log("error ocurred: ", error);
-            //res.status(400).json({"music": result });
             res.json({"code" : 400, "result": "error ocurred"});
         } else {
             console.log("music recent success");
             res.json({ "code": 200, "music": result });
-            //res.status(200).json({"music": result });
         }
     });
 });
