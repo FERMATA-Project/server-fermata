4f41e54686939e210720bc7c4480ea6af0868907
이다영 <dayoung@idayeong-ui-MacBookPro.local>
2021-08-28 20:54:13 +0900

    0828 dayoung

---
+++ routes/update_playdate.js
@@ -0,0 +1,27 @@
+// 좋아요 상태 변경
+//Author : Dayoung, Last Modified : 2021.08.27
+
+var express = require('express');
+var router = express.Router();
+var config = require('../config/db_config');
+var connection = config.init()
+connection.connect();
+
+router.post('/playdate', function (req, res) {
+    var play_date = req.body.play_date; // 재생 날짜 
+    var music_id = req.body.music_id; // 음악 아이디
+
+    var query = 'UPDATE music SET play_date = ? WHERE music_id = ?'; 
+
+    connection.query(query, [play_date, music_id], function (error, result) {
+        if(error) { // 에러 발생시
+            console.log("error ocurred: ", error);
+            res.json({"code" : 400, "result": "error ocurred"});
+        } else {
+            console.log("like success");
+            res.json({ "code": 200, "result": "update play_date success"});
+        }
+    });
+});
+
+module.exports = router;
