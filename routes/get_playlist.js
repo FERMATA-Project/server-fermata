49f3083569707b9e423749da8d160afdc64f4b24
이다영 <dayoung@idayeong-ui-MacBookPro.local>
2021-09-04 20:43:48 +0900

    0904 dayoung

--- routes/get_playlist.js
+++ routes/get_playlist.js
@@ -1,4 +1,5 @@
-// playlist 테이블에서 "현재" 재생목록 가져오기
+// playlist 테이블에서 ? 재생목록 가져오기
+//Author : dayoung, Last Modified : 2020.09.04
 
 var express = require('express');
 var router = express.Router();
@@ -7,10 +8,11 @@ var connection = config.init()
 connection.connect();
 
 
-router.post('/now', function (req, res) {
-    var query = "SELECT * FROM music WHERE music_id IN (SELECT music_id FROM playlist WHERE playlist_title = '현재')";
+router.post('/get_playlist', function (req, res) {
+    var playlist_title = req.body.playlist_title; // 선택된 재생 목록 이름
+    var query = "SELECT * FROM music WHERE music_id IN (SELECT music_id FROM playlist WHERE playlist_title = ?)";
 
-    connection.query(query, function (error, result) {
+    connection.query(query, [playlist_title], function (error, result) {
         if(error) {
             console.log("error: ", error);
             res.json({"code" : 400, "result": error});
