7156bc55a90c6481b3e7b1735a823f4da2399bfa
soohyun <soohyun6879@naver.com>
2021-09-17 21:43:25 +0900

    Update: playlist_delete _music and update_playdate

--- routes/update_playdate.js
+++ routes/update_playdate.js
@@ -1,5 +1,6 @@
-// 좋아요 상태 변경
+// 음악 재생 날짜와 재생 횟수 변경
 //Author : Dayoung, Last Modified : 2021.08.27
+//Author : Soohyun, Last Modified : 2021.09.17
 
 var express = require('express');
 var router = express.Router();
@@ -9,11 +10,12 @@ connection.connect();
 
 router.post('/playdate', function (req, res) {
     var play_date = req.body.play_date; // 재생 날짜 
+    var count = req.body.count; // 재생 횟수
     var music_id = req.body.music_id; // 음악 아이디
 
-    var query = 'UPDATE music SET play_date = ? WHERE music_id = ?'; 
+    var query = 'UPDATE music SET play_date = ?, count = ? WHERE music_id = ?'; 
 
-    connection.query(query, [play_date, music_id], function (error, result) {
+    connection.query(query, [play_date, count, music_id], function (error, result) {
         if(error) { // 에러 발생시
             console.log("error ocurred: ", error);
             res.json({"code" : 400, "result": "error ocurred"});
